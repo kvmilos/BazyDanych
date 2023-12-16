21adff3c83d553ebe1ec534df2bc6d03a852fa3a
kvmilos <kamiltomaszek@icloud.com>
2023-12-16 16:45:31 +0100

    Fix rendering issue in przyklad.html and add new route for displaying data

--- strona1/page1.py
+++ strona1/page1.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template
 import psycopg2
+import json
 
 app = Flask(__name__)
 
 conn = psycopg2.connect(database='kvmilos')
+
+@app.route("/pokaz")
+def pokaz():
+    cur = conn.cursor()
+    cur.execute("SELECT liczba FROM liczby")
+    result = [row[0] for row in cur.fetchall()]
+    return render_template('przyklad.html', x=result)
 
 @app.route("/przyklad")
 def przyklad():
